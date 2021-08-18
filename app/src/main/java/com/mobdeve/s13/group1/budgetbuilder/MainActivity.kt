7f1651e9ab44403efea0039c7d9a92d0f4f8977e
vanessa-tan09 <71954156+vanessa-tan09@users.noreply.github.com>
2021-08-18 16:44:43 +0800

    Fixed main activity layout

--- app/src/main/java/com/mobdeve/s13/group1/budgetbuilder/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/group1/budgetbuilder/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.mobdeve.s13.group1.budgetbuilder
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        //Hide navbar
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
     }
 }
