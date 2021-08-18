4787839b74c060a9ba5ff0126b31cc8e0db16c3a
vanessa-tan09 <71954156+vanessa-tan09@users.noreply.github.com>
2021-08-18 20:04:57 +0800

    Created expense activity

--- app/src/main/java/com/mobdeve/s13/group1/budgetbuilder/ExpenseActivity.kt
+++ app/src/main/java/com/mobdeve/s13/group1/budgetbuilder/ExpenseActivity.kt
@@ -1,11 +1,11 @@
 package com.mobdeve.s13.group1.budgetbuilder
 
 import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ExpenseActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
