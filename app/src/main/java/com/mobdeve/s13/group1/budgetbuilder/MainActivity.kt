f863c9502ceeab01efbf380a183e0f8da8b7acf9
vanessa-tan09 <71954156+vanessa-tan09@users.noreply.github.com>
2021-08-19 15:59:17 +0800

    Added navbar and changed activities to fragments

--- app/src/main/java/com/mobdeve/s13/group1/budgetbuilder/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/group1/budgetbuilder/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavBar()
     }
 
     override fun onResume() {
@@ -24,5 +28,11 @@ class MainActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+    }
+
+    fun initNavBar() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_navigation_view).setupWithNavController(navController)
     }
 }
